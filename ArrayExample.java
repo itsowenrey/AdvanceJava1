feb601b08e3ef7a36c4dac569ca5c6b097637abb
itsowenrey <158491910+itsowenrey@users.noreply.github.com>
2024-09-25 16:46:53 -0400

    Update ArrayExample.java

--- ArrayExample.java
+++ ArrayExample.java
@@ -1,27 +1,63 @@
 public class ArrayExample {
     public static void main(String[] args) {
-        int[] array = new int[1000000];
+        int size = 1000000;  // Adjusted for 1,000,000 elements
+        int[] array = new int[size];
 
-        // Insertion
-        long startTime = System.nanoTime();
-        for (int i = 0; i < array.length; i++) {
+        // Fill array with values
+        for (int i = 0; i < size; i++) {
             array[i] = i;
         }
-        long endTime = System.nanoTime();
-        System.out.println("Array Insertion Time: " + (endTime - startTime) + " ns");
 
-        // Search
+        // Read by Index
+        long startTime = System.nanoTime();
+        int value = array[500000];
+        long endTime = System.nanoTime();
+        System.out.println("Array Read by Index Time: " + (endTime - startTime) + " ns");
+
+        // Read by Value
         startTime = System.nanoTime();
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (array[i] == 999999) break;
         }
         endTime = System.nanoTime();
-        System.out.println("Array Search Time: " + (endTime - startTime) + " ns");
+        System.out.println("Array Read by Value Time: " + (endTime - startTime) + " ns");
 
-        // Deletion (simulated by setting to a default value)
+        // Insertion at Head (shift elements)
         startTime = System.nanoTime();
-        array[999999] = 0;
+        System.arraycopy(array, 0, array, 1, size - 1);  // Shift elements
+        array[0] = -1;
         endTime = System.nanoTime();
-        System.out.println("Array Deletion Time: " + (endTime - startTime) + " ns");
+        System.out.println("Array Insert at Head Time: " + (endTime - startTime) + " ns");
+
+        // Insertion in Middle
+        startTime = System.nanoTime();
+        System.arraycopy(array, 500000, array, 500001, size - 500001);  // Shift elements
+        array[500000] = -1;
+        endTime = System.nanoTime();
+        System.out.println("Array Insert in Mid Time: " + (endTime - startTime) + " ns");
+
+        // Insertion at Tail
+        startTime = System.nanoTime();
+        array[size - 1] = -1;  // No shifting needed
+        endTime = System.nanoTime();
+        System.out.println("Array Insert at Tail Time: " + (endTime - startTime) + " ns");
+
+        // Deletion from Head
+        startTime = System.nanoTime();
+        System.arraycopy(array, 1, array, 0, size - 1);  // Shift elements
+        endTime = System.nanoTime();
+        System.out.println("Array Delete from Head Time: " + (endTime - startTime) + " ns");
+
+        // Deletion from Middle
+        startTime = System.nanoTime();
+        System.arraycopy(array, 500001, array, 500000, size - 500001);  // Shift elements
+        endTime = System.nanoTime();
+        System.out.println("Array Delete from Mid Time: " + (endTime - startTime) + " ns");
+
+        // Deletion from Tail
+        startTime = System.nanoTime();
+        array[size - 1] = 0;  // No shifting needed
+        endTime = System.nanoTime();
+        System.out.println("Array Delete from Tail Time: " + (endTime - startTime) + " ns");
     }
 }
