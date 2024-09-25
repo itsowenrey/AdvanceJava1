c2b941f5dcc6856bf37c0837f6f8b7543bd52e29
itsowenrey <158491910+itsowenrey@users.noreply.github.com>
2024-09-25 16:47:16 -0400

    Update ArrayListExample.java

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -1,28 +1,60 @@
 import java.util.ArrayList;
 
 public class ArrayListExample {
-    @SuppressWarnings("removal")
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
 
-        // Insertion
-        long startTime = System.nanoTime();
+        // Fill ArrayList with values
         for (int i = 0; i < 1000000; i++) {
             arrayList.add(i);
         }
-        long endTime = System.nanoTime();
-        System.out.println("ArrayList Insertion Time: " + (endTime - startTime) + " ns");
 
-        // Search
+        // Read by Index
+        long startTime = System.nanoTime();
+        int value = arrayList.get(500000);
+        long endTime = System.nanoTime();
+        System.out.println("ArrayList Read by Index Time: " + (endTime - startTime) + " ns");
+
+        // Read by Value
         startTime = System.nanoTime();
         arrayList.contains(999999);
         endTime = System.nanoTime();
-        System.out.println("ArrayList Search Time: " + (endTime - startTime) + " ns");
+        System.out.println("ArrayList Read by Value Time: " + (endTime - startTime) + " ns");
 
-        // Deletion
+        // Insert at Head
         startTime = System.nanoTime();
-        arrayList.remove(new Integer(999999));
+        arrayList.add(0, -1);
         endTime = System.nanoTime();
-        System.out.println("ArrayList Deletion Time: " + (endTime - startTime) + " ns");
+        System.out.println("ArrayList Insert at Head Time: " + (endTime - startTime) + " ns");
+
+        // Insert in Mid
+        startTime = System.nanoTime();
+        arrayList.add(500000, -1);
+        endTime = System.nanoTime();
+        System.out.println("ArrayList Insert in Mid Time: " + (endTime - startTime) + " ns");
+
+        // Insert at Tail
+        startTime = System.nanoTime();
+        arrayList.add(-1);
+        endTime = System.nanoTime();
+        System.out.println("ArrayList Insert at Tail Time: " + (endTime - startTime) + " ns");
+
+        // Delete from Head
+        startTime = System.nanoTime();
+        arrayList.remove(0);
+        endTime = System.nanoTime();
+        System.out.println("ArrayList Delete from Head Time: " + (endTime - startTime) + " ns");
+
+        // Delete from Mid
+        startTime = System.nanoTime();
+        arrayList.remove(500000);
+        endTime = System.nanoTime();
+        System.out.println("ArrayList Delete from Mid Time: " + (endTime - startTime) + " ns");
+
+        // Delete from Tail
+        startTime = System.nanoTime();
+        arrayList.remove(arrayList.size() - 1);
+        endTime = System.nanoTime();
+        System.out.println("ArrayList Delete from Tail Time: " + (endTime - startTime) + " ns");
     }
 }
