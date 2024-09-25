8b49a2bc6f16b1b93236d8326050794870a95d21
itsowenrey <158491910+itsowenrey@users.noreply.github.com>
2024-09-25 16:47:32 -0400

    Update HashSetExample.java

--- HashSetExample.java
+++ HashSetExample.java
@@ -4,24 +4,30 @@ public class HashSetExample {
     public static void main(String[] args) {
         HashSet<Integer> hashSet = new HashSet<>();
 
-        // Insertion
-        long startTime = System.nanoTime();
+        // Fill HashSet with values
         for (int i = 0; i < 1000000; i++) {
             hashSet.add(i);
         }
-        long endTime = System.nanoTime();
-        System.out.println("HashSet Insertion Time: " + (endTime - startTime) + " ns");
 
-        // Search
+        // Read by Value (search)
+        long startTime = System.nanoTime();
+        hashSet.contains(500000);
+        long endTime = System.nanoTime();
+        System.out.println("HashSet Read by Value Time: " + (endTime - startTime) + " ns");
+
+        // HashSet doesn't support index-based access
+        System.out.println("HashSet doesn't support Read by Index or Insert/Delete at specific positions.");
+
+        // Insertion
         startTime = System.nanoTime();
-        hashSet.contains(999999);
+        hashSet.add(-1);
         endTime = System.nanoTime();
-        System.out.println("HashSet Search Time: " + (endTime - startTime) + " ns");
+        System.out.println("HashSet Insert Time: " + (endTime - startTime) + " ns");
 
         // Deletion
         startTime = System.nanoTime();
         hashSet.remove(999999);
         endTime = System.nanoTime();
-        System.out.println("HashSet Deletion Time: " + (endTime - startTime) + " ns");
+        System.out.println("HashSet Delete Time: " + (endTime - startTime) + " ns");
     }
 }
